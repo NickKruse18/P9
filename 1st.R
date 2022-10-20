111a8baad82463e5511d9cf14377d6f4284e25de
NickKruse18 <97922500+NickKruse18@users.noreply.github.com>
2022-10-20 11:21:09 +0200

    Add files via upload

--- 1st.R
+++ 1st.R
@@ -3,9 +3,142 @@
 #Tryk ctrl + A og ctrl + enter, i 2nd.R for at loade det hele.
 #Efter 2nd.R er loaded er det ikke nødvendigt at kigge på den mere.
 
+
 #---------------------------------------------------------------------------------------------------------------
 
-#Simuler rBergomi med Euler Maruyama
+#Simuler rBergomi med Euler Maruyama og Hybrid scheme
+
+t = 400 # mængden af skridt i simuleringen (mere akkurat simulering) 
+n = 10000 # mængden af simuleringer (mere akkurat forventet værdi E[(S-K)^+])
+eta = 0.4 # rBergomi parameter der øger volatiliteten af volatiliteten
+H = 0.1 # rBergomi parameter ændrer powerlawen på volatity skew: tau^(H-1/2)
+rho = 0.85 # korrelationen mellem den brownske bevægelse Z der hører til prisen S og den brownske bevægelse der høre til volatiliteten Xi
+sigma0 = 0.235^2 #Begyndelses volatiliteten
+S0 = 1 #Begyndelses pris
+
+
+B = rBerVol2(sigma0,eta,H,t,n,3)
+
+dW = B[[2]]
+B = B[[1]]
+plot(B%*%rep(1,n)/n)
+dZ = rho*dW+sqrt(1-rho^2)*dBM(t,n)
+
+S = BSV(S0,-0.327,1/t,B,dZ) # simulerer n paths af prisen S for en rBergomi model. kompexitet af O(n t)
+mean(S%*%rep(1,n)/n) # et problem jeg ikke har løst er at S har en smule uønsket bias afhængigt af parameterne. så -0.263 i BSV skal justeres indtil mean er lig 1
+plot(S%*%rep(1,n)/n,type = "l") # gennemsnits path for S_tau
+
+Tau = (1:20)*(t/20) # de expiration tider for de simulerede options
+K = S0*exp((-50:50)*0.01) # de strike priser for de simulerede options
+
+PSurface = PriceSurface(S,Tau,K) #Pris overfladen for forskellige options med rBergomi som underlying. Tau er expiration tider og K er strike priser
+persp(Tau,log(K/S0),PSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="P",main="Monte Carlo Price Surface") #3D plot af pris overfalden. theta og phi kontrollerer syns vinklen. log er taget til K for at få log moneyness
+VolSurface = ImpliedVol(S0,(1:20)/20,K,PSurface,0) #implied volatilitets overflade for option priserne.
+persp(Tau,log(K/S0),VolSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="V",main="Monte Carlo Implied Volatility Surface") #3D plot af volatilitets overfalden. theta og phi kontrollerer syns vinklen. log er taget til K for at få log moneyness
+FitPowerLaw(VolSurface/0.01,50,51,Tau) # volatility skewet of log(K/S0) = 0 og det bedste powerlaw fit (a tau^b). (50,51 er valgt til at matche den søjle i VolSurface der svarer til log(K/S) = 0 samt den lige ved siden af.)
+lines(Tau,rho*eta/2/(H+1/2)/(H+3/2)*sqrt(2*H)*(Tau/t)^(H-0.5),col="red")
+# Den røde linje er teoretisk volatility skew og de sorte prikker er simuleret. De matcher næsten.
+
+
+#---------------------------------------------------------------------------------------------------------------
+
+
+# udregner den implied volatility surface og skew for en given aktie.
+
+Asset = "^SPX" # Det underliggende aktiv for optionerne. Apple i dette tilfælde
+Source = "yahoo"
+
+Data = AssetVolatility(Asset,Source,3712,as.Date("2022-10-19")) #3712 og as.Date skal være pris og dato for tidspunktet dataen var taget
+S0 = Data[[3]]
+Skew = Data[[2]]
+Data = Data[[1]]
+plot(Skew) 
+
+# Volatility Smile
+for(i in Skew[,1]){
+  plot(Data[(Data[,1] == i),2],Data[(Data[,1] == i),4],main = i)
+}
+
+SData = Data[(Data[,5]>=100)&(Data[,1]<=800),] # Fjerner data med mindre end 100 volumen og mere end 800 dage til maturity
+Fit = FitModel(c(-1.36,0.7,0,0,0.31),S0,800,1000,SData,"rBer") # Fit med rBergomi model
+Fit.BS = FitModel(c(-1.36,0.02),S0,800,1000,SData,"BS") # Fit med Black-Scholes model
+#SPX (old) rBer 0.002844473. BS 0.005176576
+#AAPL rBer 0.003353342, BS 0.005057349. Resultat for AAPL data Oct 19
+#SPX rBer 0.002139801, BS 0.003452251, pris 3712. Resultat for ^SPX data Oct 19
+
+Data = readRDS("SPXData")
+Fit = readRDS("SPXFit")
+Fit.BS = readRDS("SPXFitBS")
+
+Tau = (1:20)*40 # Maturities hvor prisoverfladen er evalueret
+K = S0*exp((-50:50)*0.01) # Strikes hvor prisoverfladen er evalueret
+# Prisoverflade for rBergomi modellen
+PSurface = rBerSurface(c(exp(Fit[[1]][1]),Fit[[1]][2],0.5/(1+exp(Fit[[1]][3])),2*1/(1+exp(Fit[[1]][4]))-1,Fit[[1]][5],S0),800,1000,Tau,K)
+# Prisoverflade for Black-Scholes modellen
+PSurface.BS = BSSurface(c(exp(Fit.BS[[1]][1]),Fit.BS[[1]][2],S0),800,1000,Tau,K)
+# Prisoverflade i rød og faktiske priser (Cleaned) i sort for rBergomi
+points(trans3d(Fit[[3]][,1],log(Fit[[3]][,2]/S0),Fit[[3]][,3],pmat = persp(Tau,log(K/S0),PSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="P",main="rBergomi Price Surface (Cleaned Data)",border="red")))
+# Prisoverflade i rød og faktiske priser (Cleaned) i sort for Black-Scholes
+points(trans3d(Fit.BS[[3]][,1],log(Fit.BS[[3]][,2]/S0),Fit.BS[[3]][,3],pmat = persp(Tau,log(K/S0),PSurface.BS,theta=120,phi=30,xlab="tau",ylab="k",zlab="P",main="Black-Scholes Price Surface (Cleaned Data)",border="red")))
+# Prisoverflade i rød og alle faktiske priser (SData) i sort for rBergomi
+points(trans3d(SData[,1],log(SData[,2]/S0),SData[,3],pmat = persp(Tau,log(K/S0),PSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="P",main="rBergomi Price Surface (Uncleaned Data)",border="red")))
+
+
+#---------------------------------------------------------------------------------------------------------------
+#Heston Model
+t = 400
+n = 100000
+eta = 0.4
+H = 0.1
+rho = 0.85
+sigma0 = 0.235^2
+S0 = 1
+
+
+dZ = dBM(t,n)
+dW = rho*dZ+sqrt(1-rho^2)*dBM(t,n)
+
+H = HesVol(sigma0,eta,dW)
+
+plot(H%*%rep(1,n)/n,type = "l")
+
+S = BSV(S0,-0.168,1/t,H,dZ)
+mean(S%*%rep(1,n)/n)
+plot(S%*%rep(1,n)/n,type = "l")
+
+Tau = (1:20)*(t/20)
+K = S0*exp((-50:50)*0.01)
+
+PSurface = PriceSurface(S,Tau,K)
+persp(Tau,log(K/S0),PSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="P",main="Heston Model Price Surface")
+VolSurface = ImpliedVol(S0,(1:20)/20,K,PSurface,0)
+persp(Tau,log(K/S0),VolSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="V",main="Heston Model Implied Volatility Surface")
+FitPowerLaw(VolSurface/0.01,50,51,Tau,"Heston Model Volatility Skew")
+
+#---------------------------------------------------------------------------------------------------------------
+#Delta Hedging
+
+t = 800 # mængden af skridt i simuleringen (mere akkurat simulering) 
+n = 1000 # mængden af simuleringer (mere akkurat forventet værdi E[(S-K)^+])
+eta = 0.4 # rBergomi parameter der øger volatiliteten af volatiliteten
+H = 0.1 # rBergomi parameter ændrer powerlawen på volatity skew: tau^(H-1/2)
+rho = 0.85 # korrelationen mellem den brownske bevægelse Z der hører til prisen S og den brownske bevægelse der høre til volatiliteten Xi
+sigma0 = 0.235^2 #Begyndelses volatiliteten
+S0 = 1 #Begyndelses pris
+
+dW = dBM(t,n)
+dZ = dBM(t,n)
+S = BSV(S0,0,1/t,matrix(sigma0,t,n),dZ)
+
+V = Hedging(S,1,S,c())
+sort(V)[n*0.95]
+sort(V)[n*0.05]
+hist(V)
+mean(V)
+var(V)
+
+#---------------------------------------------------------------------------------------------------------------
+#Benchmark
 
 t = 400 # mængden af skridt i simuleringen (mere akkurat simulering) 
 n = 1000 # mængden af simuleringer (mere akkurat forventet værdi E[(S-K)^+])
@@ -15,20 +148,41 @@ rho = 0.85 # korrelationen mellem den brownske bevægelse Z der hører til prise
 sigma0 = 0.235^2 #Begyndelses volatiliteten
 S0 = 1 #Begyndelses pris
 
+mark = microbenchmark("HS t = 100" = {rBerVol2(sigma0,eta,H,100,n,3)},"EM t = 100" = {rBerVol(sigma0,eta,H,100,n)},
+                      "HS t = 400" = {rBerVol2(sigma0,eta,H,400,n,3)},"EM t = 400" = {rBerVol(sigma0,eta,H,400,n)},
+                      "HS t = 1000" = {rBerVol2(sigma0,eta,H,1000,n,3)},"EM t = 1000" = {rBerVol(sigma0,eta,H,1000,n)})
+microbenchmark("HS t = 100" = {rBerVol2(sigma0,eta,H,100,n,3)},
+               "HS t = 400" = {rBerVol2(sigma0,eta,H,400,n,3)},
+               "HS t = 1000" = {rBerVol2(sigma0,eta,H,1000,n,3)})
+mark
 
-dZ = dBM(t,n) #t X n matrice af normalt fordelte værdier til at simulere Z_{tau/t}-Z_{(tau-1)/t}
-dW = rho*dZ+sqrt(1-rho^2)*dBM(t,n) # samme som dZ, dog med en korrelation til dZ på rho
+#---------------------------------------------------------------------------------------------------------------
 
-b = rBerVol(sigma0,eta,H,dW) # simulerer n paths af volatiliten Xi for en rBergomi model. Meste af processer tiden: kompexitet af O(n t^2)
+#Simuler rBergomi med Euler Maruyama
+
+t = 400 # mængden af skridt i simuleringen (mere akkurat simulering) 
+n = 100 # mængden af simuleringer (mere akkurat forventet værdi E[(S-K)^+])
+eta = 0.4 # rBergomi parameter der øger volatiliteten af volatiliteten
+H = 0.1 # rBergomi parameter ændrer powerlawen på volatity skew: tau^(H-1/2)
+rho = 0.85 # korrelationen mellem den brownske bevægelse Z der hører til prisen S og den brownske bevægelse der høre til volatiliteten Xi
+sigma0 = 0.235^2 #Begyndelses volatiliteten
+S0 = 1 #Begyndelses pris
+
+
+
+
+b = rBerVol(sigma0,eta,H,t,n) # simulerer n paths af volatiliten Xi for en rBergomi model. Meste af processer tiden: kompexitet af O(n t^2)
 B = b[[1]] # Xi_tau(tau) for tau = 1/t, 2/t, ..., (t-1)/t, 1
+dW = b[[3]]
 b = b[[2]] # Xi_tau(1)
+dZ = rho*dW+sqrt(1-rho^2)*dBM(t,n) # samme som dZ, dog med en korrelation til dZ på rho
 plot(B%*%rep(1,n)/n,type = "l") # gennemsnits path for Xi_tau(tau)
 plot(b%*%rep(1,n)/n,type = "l") # gennemsnits path for Xi_tau(1)
-S = BSV(S0,-0.263,1/t,B,dZ) # simulerer n paths af prisen S for en rBergomi model. kompexitet af O(n t)
+S = BSV(S0,-0.198,1/t,B,dZ) # simulerer n paths af prisen S for en rBergomi model. kompexitet af O(n t)
 mean(S%*%rep(1,n)/n) # et problem jeg ikke har løst er at S har en smule uønsket bias afhængigt af parameterne. så -0.263 i BSV skal justeres indtil mean er lig 1
 plot(S%*%rep(1,n)/n,type = "l") # gennemsnits path for S_tau
 
-plot(CoVT(diff(log(S)),diff(b))) 
+plot(CoVT(diff(log(S)),diff(b)),xlab = expression(tau),ylab=expression(paste("E[d",x[tau],"d", xi[tau](u),"]/dt")),main=expression(paste("Monte Carlo vs Theoretical E[d",x[tau],"d", xi[tau](u),"]/dt"))) 
 lines(rho*eta*sqrt(2*H)*sqrt(B[1:(t-1),]%*%rep(1,n)/n)*b[1:(t-1),]%*%rep(1,n)/n*((t-1:(t-1))/t)^(H-0.5)/t, col = "red")
 # Et egenskaberne for rBergomi er at de sorte prikker (simuleret) skal match den røde linje (teoretisk), (ligningen kan ses på s. 898 ligningen over 7.1. Special case...)
 
@@ -36,31 +190,11 @@ Tau = (1:20)*(t/20) # de expiration tider for de simulerede options
 K = S0*exp((-50:50)*0.01) # de strike priser for de simulerede options
 
 PSurface = PriceSurface(S,Tau,K) #Pris overfladen for forskellige options med rBergomi som underlying. Tau er expiration tider og K er strike priser
-persp(Tau,log(K/S0),PSurface,theta=120,phi=30) #3D plot af pris overfalden. theta og phi kontrollerer syns vinklen. log er taget til K for at få log moneyness
+persp(Tau,log(K/S0),PSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="P",main="Monte Carlo Price Surface") #3D plot af pris overfalden. theta og phi kontrollerer syns vinklen. log er taget til K for at få log moneyness
 VolSurface = ImpliedVol(S0,(1:20)/20,K,PSurface,0) #implied volatilitets overflade for option priserne.
-persp(Tau,log(K/S0),VolSurface,theta=120,phi=30) #3D plot af volatilitets overfalden. theta og phi kontrollerer syns vinklen. log er taget til K for at få log moneyness
+persp(Tau,log(K/S0),VolSurface,theta=120,phi=30,xlab="tau",ylab="k",zlab="V",main="Monte Carlo Implied Volatility Surface") #3D plot af volatilitets overfalden. theta og phi kontrollerer syns vinklen. log er taget til K for at få log moneyness
 FitPowerLaw(VolSurface/0.01,50,51,Tau) # volatility skewet of log(K/S0) = 0 og det bedste powerlaw fit (a tau^b). (50,51 er valgt til at matche den søjle i VolSurface der svarer til log(K/S) = 0 samt den lige ved siden af.)
 lines(Tau,rho*eta/2/(H+1/2)/(H+3/2)*sqrt(2*H)*(Tau/t)^(H-0.5),col="red")
 # Den røde linje er teoretisk volatility skew og de sorte prikker er simuleret. De matcher næsten.
 
-#---------------------------------------------------------------------------------------------------------------
-
-# udregner den implied volatility surface og skew for en given aktie.
-
-Asset = "AAPL" # Det underliggende aktiv for optionerne. Apple i dette tilfælde
-
-X = getSymbols(Asset,src="yahoo",auto.assign = F)[length(getSymbols(Asset,src="yahoo",auto.assign = F)[,1]),6] # handelsprisen og dato
-date = index(X) # Isolere dato
-X = X[[1]] # Isolere pris
-D = getOptionChain(Asset,Exp=NULL,src="yahoo") # Hent optionerne for aktivet
-r = 0.00000 # risk neutral rate
-
-
-Data = ProcessOptions(D,date) # Konverterer det til en matrice med de nødvendige data, (dage til expiration, strike pris, og options pris)
-Data = AddImpliedVol(Data,X,r) # Tilføjer implied vol. 
-
-Surface = InterpolateSurface(Data[,1],Data[,2],Data[,3]) # Pris surface for optionerne. For at få en ordenlig surface er det nødvendigt at interpolere nogle værdier, så jeg lavede en hurtig funktion til det.
-Surface = InterpolateSurface(Data[,1],Data[,2],Data[,4]) # Implied vol surface
-FitPowerLaw(Surface[[3]],44,45,Surface[[1]]) # volatility skewet of log(K/S0) = 0 og det bedste powerlaw fit (a tau^b). (44,45 er valgt til at matche den søjle i VolSurface der svarer til log(K/S) = 0 samt den lige ved siden af. Se næste linjes Surface[[2]] for at finde de bedste indeks)
-Surface[[2]] #Strike pris K for forskellige søjler i implied Vol surface
 
--- 2nd.R
+++ 2nd.R
@@ -1,13 +1,27 @@
 library("quantmod")
+library("stats")
+library("hypergeo")
+library("microbenchmark")
+
 
 dBM = function(t,n){
   return(matrix(rnorm(t*n),t,n)/sqrt(t))
 }
 
-
-rBerVol = function(sigma,eta,H,dW){
+HesVol = function(sigma,eta,dW){
   t = length(dW[,1])
   n = length(dW[1,])
+  V = matrix(sigma,t,n)
+  for(i1 in 1:t){
+    for(i2 in 1:n){
+      V[i1,i2] = V[i1,i2] + eta*sqrt(V[i1,i2])*dW[i1,i2]
+    }
+  }
+  return(V)
+}
+
+rBerVol = function(sigma,eta,H,t,n){
+  dW = dBM(t,n)
   V = matrix(sigma,t,n)
   v = matrix(sigma,t,n)
   for(i1 in 1:t){
@@ -24,18 +38,19 @@ rBerVol = function(sigma,eta,H,dW){
       v[i2,i3] = v[i2-(i2>1),i3] + v[i2-(i2>1),i3]*gamma*dW[i2,i3]
     }
   }
-  return(list(V,v))
+  return(list(V,v,dW))
 }
 
 BSV = function(S0,mu,dt,sigma,dZ){
   t = length(dZ[,1])
   n = length(dZ[1,])
-  S = matrix(S0,t,n)
+  S = matrix(0,t,n)
   for (i1 in 1:t){
     for (i2 in 1:n){
-      S[i1,i2] = S[i1-(i1>1),i2] + S[i1-(i1>1),i2]*(mu*dt + sqrt(sigma[i1,i2])*dZ[i1,i2])
+      S[i1,i2] = S[i1-(i1>1),i2] + (mu-sigma[i1,i2]/2)*dt + sqrt(sigma[i1,i2])*dZ[i1,i2]
     }
   }
+  S = S0*exp(S)
   return(S)
 }
 
@@ -80,9 +95,11 @@ ImpliedVol = function(S,tau,K,Surface,mu){
   return(VolSurface)
 }
 
-FitPowerLaw = function(Surface,start,end,tau){
-  y = Surface[,end] - Surface[,start]
-  plot(tau,y)
+FitPowerLaw = function(Surface,start,end,tau,titel = ""){
+  if(end == 0){ y = Surface }
+  else{ y = Surface[,end] - Surface[,start] }
+  
+  plot(tau,y,xlab = expression(tau),ylab=expression(psi(tau)),main = titel)
   s = sign(mean(exp(-c(1:length(y))/2)*y))
   Y = y*s
   X = cbind(1,log(tau[(Y>0)]))
@@ -94,15 +111,21 @@ FitPowerLaw = function(Surface,start,end,tau){
 
 ProcessOptions = function(D,date){
   n = 0
-  for (i1 in 1:length(D)){
-    n = n + length(D[[i1]]$calls[,1])
+  for (i in 1:length(D)){
+    if (length(D[[i]]$calls[,1])==0){next}
+    if (difftime(D[[i]]$calls[1,4],paste(date," 02:00:00"))[[1]]>1000){next}
+    if (difftime(D[[i]]$calls[1,4],paste(date," 02:00:00"))[[1]] == 0){next}
+    n = n + length(D[[i]]$calls[,1])
   }
-  Data = matrix(0,n,3)
+  Data = matrix(0,n,5)
   i = 0
   for (i1 in 1:length(D)){
+    if (length(D[[i1]]$calls[,1])==0){next}
+    if (difftime(D[[i1]]$calls[1,4],paste(date," 02:00:00"))[[1]]>1000){next}
+    if (difftime(D[[i1]]$calls[1,4],paste(date," 02:00:00"))[[1]] == 0){next}
     for (i2 in 1:length(D[[i1]]$calls[,1])){
       i = i + 1
-      Data[i,] = c(difftime(D[[i1]]$calls[i2,4],paste(date," 02:00:00"))[[1]],D[[i1]]$calls[i2,5],(D[[i1]]$calls[i2,9]+D[[i1]]$calls[i2,10])/2)
+      Data[i,] = c(difftime(D[[i1]]$calls[i2,4],paste(date," 02:00:00"))[[1]],D[[i1]]$calls[i2,5],D[[i1]]$calls[i2,9],D[[i1]]$calls[i2,14],D[[i1]]$calls[i2,12])
       Data[is.na(Data)] = 0
     }
   }
@@ -141,4 +164,206 @@ InterpolateSurface = function(X,Y,Z){
   }
   persp(Xs,Ys,Zs/Zw,theta=120,phi=30)
   return(list(Xs,Ys,Zs/Zw))
+}
+
+AssetVolatility = function(Asset,Source,X,date){
+  D = getOptionChain(Asset,Exp=NULL,src=Source)
+  
+  Data = ProcessOptions(D,date)
+  #Data = AddImpliedVol(Data,X,r/365)
+  Surface = InterpolateSurface(Data[,1],Data[,2],Data[,3])
+  Surface = InterpolateSurface(Data[,1],Data[,2],Data[,4])
+  
+  s = 1
+  for(i in (1:length(Surface[[2]]))){
+    if(Surface[[2]][i] > X){
+      s = i
+      break
+    }
+  }
+  print(FitPowerLaw(Surface[[3]],s-1+12,s+12,Surface[[1]]))
+  return(list(Data,cbind(Surface[[1]],Surface[[3]][,s+12]-Surface[[3]][,s-1+12]),X))
+}
+
+rBerVol2 = function(sigma,eta,H,t,n,k){
+  Y = matrix(sigma,t,n)
+  Sigma = matrix(1/t,k+1,k+1)
+  for(i in 2:(k+1)){
+    Sigma[i,i] = ((i-1)^(2*H)-(i-2)^(2*H))/((2*H)*t^(2*H))
+    Sigma[1,i] = ((i-1)^(H+1/2)-(i-2)^(H+1/2))/((H+1/2)*t^(H+1/2))
+    Sigma[i,1] = Sigma[1,i]
+    if(i == 2){next}
+    for(j in 2:(i-1)){
+      Sigma[i,j] = ((j-1)^(H+1/2)*(i-1)^(H-1/2)*Re(hypergeo(1/2-H,1,H+3/2,(j-1)/(i-1))))/((H+1/2)*t^(2*H))
+      Sigma[i,j] = Sigma[i,j] - ((j-2)^(H+1/2)*(i-2)^(H-1/2)*Re(hypergeo(1/2-H,1,H+3/2,(j-2)/(i-2))))/((H+1/2)*t^(2*H))
+      Sigma[j,i] = Sigma[i,j]
+    }
+  }
+  Sigma = t(chol(Sigma))
+  set.seed(2)
+  dW = Sigma%*%matrix(rnorm((k+1)*t*n),k+1,t*n)
+  g = c(rep(0,k), (((k+1):t)/t)^(H-1/2),rep(0,t))
+  G = fft(g)
+  dWW = dW[2:(k+1),]
+  dW = dW[1,]
+  for(i in 2:k){
+    dWW[i,] = c(rep(0,i-1),dWW[i,1:(t*n-i+1)])
+  }
+  for(i1 in 1:(n-1)){
+    for(i2 in 2:k){
+      for(i3 in 1:(i2-1)){
+        dWW[i2,i1*t+i3] = 0
+      }
+    }
+  }
+  V = matrix(rep(1,k)%*%dWW,t,n)
+  for(i in 1:n){
+    DW = fft(c(rep(0,t),dW[t*i-t+(1:t)]))
+    V[,i] = V[,i] + round(Re(fft(DW*G,inverse=TRUE)[(t+1):(2*t)])/t/2,9)
+  }
+  
+  
+  V = sigma*exp(eta*sqrt(2*H)*V-eta^2/2*(((1:t)/t)^(2*H))%*%t(rep(1,n)))
+  return(list(V,matrix(dW,t,n)))
+}
+
+ModelDelta = function(S0,K,mS,tau){
+  n = length(mS[1,])
+  t = length(mS[,1])
+  delta = mean(0.0002*mS[t,]/mS[tau,]+abs((S0+0.0001)*mS[t,]/mS[tau,]-K)-abs((S0-0.0001)*mS[t,]/mS[tau,]-K))/0.0004
+  return(delta)
+}
+
+CallOpt = function(S0,K,mS,tau){
+  n = length(mS[1,])
+  t = length(mS[,1])
+  return(mean(S0*S[t,]/S[tau,]-K+abs(S0*S[t,]/S[tau,]-K))/2)
+}
+
+Hedging = function(S,K,mS,tau){
+  n = length(mS[1,])
+  t = length(mS[,1])
+  Positions = rep(0,n)
+  Cost = rep(-CallOpt(mean(S[1,]),K,mS,1),n)
+  for(i1 in tau){
+    M = (0:99)*(max(S[i1,])-min(S[i1,]))/99+min(S[i1,])
+    Delta = rep(0,100)
+    for(i2 in 1:100){
+      Delta[i2] = ModelDelta(M[i2],K,mS,i1)
+    }
+    for(i2 in 1:n){
+      for(i3 in 1:100){ if(M[i3] > S[i1,i2]){ break } }
+      delta = Delta[i3-1] + (M[i3] - S[i1,i2])/(M[i3] - M[i3-1]) * (Delta[i3] - Delta[i3-1])
+      Cost[i2] = Cost[i2] - S[i1,i2]*(delta + Positions[i2])
+      Positions[i2] = -delta
+    }
+  }
+  for(i2 in 1:n){
+    Cost[i2] = Cost[i2] - S[t,i2]*Positions[i2] + max(0,S[t,i2]-K)
+  }
+  return(Cost)
+}
+
+rBerSurface = function(H,t,n,Tau,K){
+  sigma0 = H[1]
+  eta = H[2]
+  rho = H[4]
+  r = H[5]
+  S0 = H[6]
+  H = H[3]
+  B = rBerVol2(sigma0,eta,H,t,n,3)
+  dW = B[[2]]
+  B = B[[1]]
+  dZ = rho*dW+sqrt(1-rho^2)*dBM(t,n)
+  S = BSV(S0,r,1/t,B,dZ)
+  PSurface = PriceSurface(S,Tau,K)
+  return(PSurface)
+}
+
+BSSurface = function(H,t,n,Tau,K){
+  sigma = H[1]
+  r = H[2]
+  S0 = H[3]
+  dZ = dBM(t,n)
+  S = BSV(S0,r,1/t,matrix(sigma,t,n),dZ)
+  PSurface = PriceSurface(S,Tau,K)
+  return(PSurface)
+}
+
+PriceVal = function(S,t,K){
+  Surface = rep(0,length(t))
+  for(i in 1:length(t)){
+    Surface[i] = mean((abs(S[t[i],]-K[i])+S[t[i],]-K[i])/2)
+  }
+  return(Surface)
+}
+
+CleanData = function(Fit,SData){
+  Residuals = Fit-SData[,3]
+  lims = boxplot(Residuals)$stats[c(1,5)]
+  lims = c(min(lims[1],sort(Residuals)[floor(length(Residuals)*0.025)]),max(lims[2],sort(Residuals)[ceiling(length(Residuals)*0.975)]))
+  exclude = c()
+  for(i in 1:length(Residuals)){
+    if(Residuals[i] < lims[1]){
+      exclude = c(exclude,i)
+    }
+    if(Residuals[i] > lims[2]){
+      exclude = c(exclude,i)
+    }
+  }
+  return(SData[-exclude,])
+}
+
+OptBS = function(H,S0,t,n,SData,Est=FALSE){
+  print(H)
+  set.seed(2)
+  dZ = dBM(t,n)
+  S = BSV(S0,H[2],1/t,matrix(exp(H[1]),t,n),dZ)
+  PEst = PriceVal(S,SData[,1],SData[,2])
+  if(Est){return(PEst)}
+  else{return(sum((PEst-SData[,3])^2))}
+}
+
+OptrBer = function(H,S0,t,n,SData,Est=FALSE){
+  print(H)
+  if(abs(H[3])>6){ return(Inf) }
+  B = rBerVol2(exp(H[1]),H[2],0.5/(1+exp(H[3])),t,n,3)
+  dW = B[[2]]
+  B = B[[1]]
+  G = 2*1/(1+exp(H[4]))-1
+  dZ = G*dW+sqrt(1-G^2)*dBM(t,n)
+  S = BSV(S0,H[5],1/t,B,dZ)
+  
+  PEst = PriceVal(S,SData[,1],SData[,2])
+  if(Est){return(PEst)}
+  else{return(sum((PEst-SData[,3])^2))}
+}
+
+FitModel = function(H,S0,t,n,SData,model){
+  if (model == "BS"){
+    Model = optim(H,OptBS,S0=S0,t=t,n=n,SData=SData,control = list(maxit=100))
+    Fit = OptBS(Model$par,S0,t,n,SData,TRUE)
+    print(1)
+    SData.clean = CleanData(Fit,SData)
+    Model = optim(Model$par,OptBS,S0=S0,t=t,n=n,SData=SData.clean)
+    Fit = OptBS(Model$par,S0,t,n,SData.clean,TRUE)
+  }
+  else if (model == "rBer"){
+    Model = optim(H,OptrBer,S0=S0,t=t,n=n,SData=SData,control = list(maxit=100))
+    Fit = OptrBer(Model$par,S0,t,n,SData,TRUE)
+    print(1)
+    SData.clean = CleanData(Fit,SData)
+    Model = optim(Model$par,OptrBer,S0=S0,t=t,n=n,SData=SData.clean)
+    Fit = OptBS(Model$par,S0,t,n,SData.clean,TRUE)
+  }
+  else if (model == "Hes"){
+    Model = optim(H,OptHes,S0=S0,t=t,n=n,SData=SData,control = list(maxit=100))
+    Fit = OptHes(Model$par,S0,t,n,SData,TRUE)
+    print(1)
+    SData.clean = CleanData(Fit,SData)
+    Model = optim(Model$par,OptHes,S0=S0,t=t,n=n,SData=SData.clean)
+    Fit = OptBS(Model$par,S0,t,n,SData.clean,TRUE)
+  }
+  print(sqrt(Model$value/length(SData[,3]))/S0)
+  return(list(Model$par,Fit,SData.clean))
 }

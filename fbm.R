1d8617248ef20bcd9be452b15dc667adadbd010c
Thorkild <tbends13@student.aau.dk>
2022-11-18 23:16:53 +0100

    changes

--- fbm.R
+++ fbm.R
@@ -1,18 +1,15 @@
-download.file(url = "https://cran.r-project.org/src/contrib/Archive/somebm/somebm_0.1.tar.gz", destfile = "somebm_0.1.tar.gz")
-
-install.packages(pkgs="somebm_0.1.tar.gz", type="source", repos=NULL)
 
 library("somebm")
+
+library("rumidas")
 
 FBM=fbm(hurst=0.2,n=1000)
 plot(FBM)
 
-
-library("rumidas")
-
+data=rv5
 head(rv5)
 summary(rv5)
-plot(rv5)
+plot(rv5[])
 data=rv5
 
 
